e14a60e72580647f9409188ca1dff151e917ec14
Antonfilka <antonfilka@tut.by>
2021-12-07 02:27:26 +0300

    Preloader

--- src/Redux/users-reducer.js
+++ src/Redux/users-reducer.js
@@ -3,12 +3,14 @@ const UNFOLLOW = 'UNFOLLOW';
 const SET_USERS = 'SET_USERS';
 const SET_CURRENT_PAGE = 'SET_CURRENT_PAGE';
 const SET_TOTAL_USERS = 'SET_TOTAL_USERS';
+const SET_IS_FETCHING = 'SET_IS_FETCHING';
 
 let initialState = {
     users: [ ],
     currentPage: 1,
-    screenSize: 5,
-    totalUsers: 30
+    screenSize: 20,
+    totalUsers: 30,
+    isFetching: true
 };
 
 export const usersReducer = (state = initialState, action) => {
@@ -42,16 +44,19 @@ export const usersReducer = (state = initialState, action) => {
         case SET_CURRENT_PAGE: {
             return {...state, currentPage: action.currentPage}
         }
+        case SET_IS_FETCHING:
+            return {...state, isFetching: action.isFetching}
         default:
             return state;
     }
 }
 
 
-export const followAC = (userId) => ({type: FOLLOW, userId })
-export const unflAC = (userId) => ({type: UNFOLLOW, userId })
-export const setUsersAC = (users) => ({type: SET_USERS, users })
-export const setCurrPageAC = (currentPage) => ({type: SET_CURRENT_PAGE, currentPage })
-export const setTotalUsersAC = (totalUsers) => ({type: SET_TOTAL_USERS, totalUsers })
+export const follow = (userId) => ({type: FOLLOW, userId })
+export const unfollow = (userId) => ({type: UNFOLLOW, userId })
+export const setUsers = (users) => ({type: SET_USERS, users })
+export const setCurrentPage = (currentPage) => ({type: SET_CURRENT_PAGE, currentPage })
+export const setTotalUsers = (totalUsers) => ({type: SET_TOTAL_USERS, totalUsers })
+export const setIsFetching = (isFetching) => ({type: SET_IS_FETCHING, isFetching })
 
 export default usersReducer;
---
+++ src/common/Preloader/Preloader.jsx
@@ -0,0 +1,12 @@
+import preloader from './preloader.svg';
+import styles from './Preloader.module.css'
+
+const Preloader = () => {
+    return (
+        <div className={styles.preloader}>
+            <img src={preloader} alt=''/>
+        </div>
+    )
+}
+
+export default Preloader;
