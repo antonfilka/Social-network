0e6651f8c6ed0fe1d4779ce9a2e59ac25812f42b
Antonfilka <antonfilka@tut.by>
2022-01-19 10:52:13 +0300

    Button disable

--- src/components/Users/UsersContainer.jsx
+++ src/components/Users/UsersContainer.jsx
@@ -1,6 +1,14 @@
 
 import {connect} from "react-redux";
-import {follow, setCurrentPage, setIsFetching, setTotalUsers, setUsers, unfollow} from "../../Redux/users-reducer";
+import {
+    follow,
+    setCurrentPage,
+    setIsFetching,
+    setSubscribing,
+    setTotalUsers,
+    setUsers,
+    unfollow
+} from "../../Redux/users-reducer";
 import UsersC from "./UsersC";
 
 
@@ -10,9 +18,10 @@ let mapStateToProps = (state) => {
         currentPage: state.usersPage.currentPage,
         totalUsers: state.usersPage.totalUsers,
         screenSize: state.usersPage.screenSize,
-        isFetching: state.usersPage.isFetching
+        isFetching: state.usersPage.isFetching,
+        subscribing: state.usersPage.subscribing
     }
 }
 
 export const UsersContainer = connect(mapStateToProps,
-    {follow, unfollow, setUsers, setTotalUsers, setCurrentPage, setIsFetching})(UsersC);
+    {follow, unfollow, setUsers, setTotalUsers, setCurrentPage, setIsFetching, setSubscribing})(UsersC);
